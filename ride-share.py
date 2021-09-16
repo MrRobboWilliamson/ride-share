00691ad0b6acf71a59b13a229fd3805655169682
MrRobboWilliamson <robmwilliamson@gmail.com>
2021-09-16 20:01:33 +1000

    done a whole bunch of GIS

---
+++ ride-share.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+This is the model
+"""
+
+
+## Our datasets will be the trip data
+# I have downloaded the May 2013 (not 2015) data and 
+# will filter this for the week starting Sunday 5 May to end Saturday May 11
+# this will need to pre-processed to snap request to nearest intersection,
+# within 100m - throw away the rest and report
