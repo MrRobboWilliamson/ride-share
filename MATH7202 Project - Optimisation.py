111e690c2787683e232784c0935e420be70d0b1e
TroyForster <37261078+TroyForster@users.noreply.github.com>
2021-09-25 11:22:34 +1000

    Update MATH7202 Project - Optimisation.py

--- MATH7202 Project - Optimisation.py
+++ MATH7202 Project - Optimisation.py
@@ -9,28 +9,31 @@ from gurobipy import *
 
 Taxis = [1,2]# Our taxis t
 Passengers = ['A','B','C','D']# Our passengers p
-RequestCombos = ['A','B','C','D','AD','BC','BD']# Our request combinations rc
+RequestCombos = ['A','B','C','D','AD','BC','BD', 'Unallocated']# Our request combinations rc
 
 T = range(len(Taxis))
 P = range(len(Passengers))
 RC = range(len(RequestCombos))
+RC_real = range(len(RequestCombos)-1) # our ride combos minus the unallocated node
+u_node = len(RequestCombos)-1 # index of the unallocated node
 
-max_psg = 2
+max_psg = 2 # maximum passengers per taxi
+unallocated_cost = 5 # arbitrary cost of not allocating a passenger
 
 # Our sharability graph costs showing total additional trip time if taxi t 
 # is assigned ride combination rc
 TripTimes = [
-    [999, 2, 1, 999, 999, 4, 999],
-    [4, 2, 1, 2, 2, 6, 2]
+    [999, 2, 1, 999, 999, 4, 999, 0],
+    [4, 2, 1, 2, 2, 6, 2, 0]
     ]
 
 # Our sharability graph possible assignments of passenger p to  
 # ride combination rc
 Combos = [
-    [1, 0, 0, 0, 1, 0, 0],
-    [0, 1, 0, 0, 0, 1, 1],
-    [0, 0, 1, 0, 0, 1, 0],
-    [0, 0, 0, 1, 1, 0, 1]
+    [1, 0, 0, 0, 1, 0, 0, 1],
+    [0, 1, 0, 0, 0, 1, 1, 1],
+    [0, 0, 1, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0, 1, 1]
     ]
 m = Model('Taxi Allocations')
 
@@ -38,18 +41,20 @@ X = {(t,rc): m.addVar(vtype=GRB.BINARY) for t in T for rc in RC}
 Y = {(p,rc): m.addVar(vtype=GRB.BINARY) for p in P for rc in RC}
 
 
-m.setObjective(quicksum(X[t,rc]*TripTimes[t][rc] 
-                               for t in T for rc in RC), GRB.MINIMIZE)
+m.setObjective(quicksum(X[t,rc]*TripTimes[t][rc] for t in T for rc in RC) +
+               quicksum(Y[p,u_node]*unallocated_cost 
+                               for p in P), GRB.MINIMIZE)
 
 for t in T:
-    # each taxi must take only one request combo
-    m.addConstr(quicksum(X[t,rc] for rc in RC) == 1)
+    # each taxi must take only one "real" request combo
+    m.addConstr(quicksum(X[t,rc] for rc in RC_real) == 1)
+    # taxis are never allocated to the "unallocated" node
+    m.addConstr(X[t,u_node] == 0)
 
-
-for rc in RC:
-    # each ride combo can't have more than one taxi allocated
+for rc in RC_real:
+    # each "real" ride combo can't have more than one taxi allocated
     m.addConstr(quicksum(X[t,rc] for t in T) <= 1)
-    # each ride combo can only be used by passengers if  taxi is  allocated
+    # each "real" ride combo can only be used by passengers if  taxi is allocated
     m.addConstr(quicksum(Y[p,rc] for p in P) <= 
                 quicksum(X[t,rc] for t in T)*max_psg)   
             
